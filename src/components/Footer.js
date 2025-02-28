82e3c9c34b2a2881942f2d7a8a4b530cf095c0ab
Larry Wen <wenl@pcrb.com>
2025-02-28 09:24:59 -0500

    updated Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,13 +6,13 @@ export const Footer = () => {
           <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2030 <Link to="/" className="hover:underline">Cinemate</Link>. All Rights Reserved.</span>
           <ul className="flex flex-wrap items-center mt-3 text-sm text-gray-500 dark:text-gray-400 sm:mt-0">
               <li>
-                  <a href="https://www.instagram.com/#" target="_blank" rel="noreferrer" className="mr-4 hover:underline md:mr-6 ">Instagram</a>
+                  <a href="https://www.instagram.com" target="_blank" rel="noreferrer" className="mr-4 hover:underline md:mr-6 ">Instagram</a>
               </li>
               <li>
                   <a href="https://www.linkedin.com" target="_blank" rel="noreferrer" className="mr-4 hover:underline md:mr-6">LinkedIn</a>
               </li>
               <li>
-                  <a href="https://twitter,com" target="_blank" rel="noreferrer" className="mr-4 hover:underline md:mr-6">Youtube</a>
+                  <a href="https://twitter.com" target="_blank" rel="noreferrer" className="mr-4 hover:underline md:mr-6">Twitter</a>
               </li>
               <li>
                   <a href="https://github.com" target="_blank" rel="noreferrer" className="hover:underline">Github</a>
